33e1e13dc670db81763de6ab76b3b25556b074e6
pisi774 <pisi774@gmail.com>
2025-04-11 16:02:40 -0400

    Atualização de script
    Atualização de script

--- segundo projeto/segundo-projeto/mundo.gd
+++ segundo projeto/segundo-projeto/mundo.gd
@@ -1,5 +1,7 @@
 extends Node
 
+@export	var object_templates: Array[PackedScene]
+
 #func _ready() -> void:
 	#pass
 	#
@@ -11,8 +13,13 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index ==1:
 			if event.pressed:
-				spaw_object(event.postion)
+				spaw_object(event.position)
 			
 
-func spaw_object() -> void:
-	pass
+func spaw_object(position: Vector2) -> void:
+	var index: int = randi_range(0, object_templates.size() -1)
+	var object_template = object_templates[index]
+	var object: RigidBody2D = object_template.instantiate()
+	object.position = position
+	add_child(object)
+	
